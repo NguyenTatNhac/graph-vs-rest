3722b6f4c5f9ec0fedc3cc1f12bc14da8bf31b5d
Nhac Tat Nguyen <nhac.tat.nguyen@mgm-tp.com>
2020-04-18 20:45:34 +0700

    Remove material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
       <Switch>
         <Route path="/" component={Home} exact/>
-        <Route path="/login" component={Login}/>
-        <Route path="/signup" component={Signup}/>
         <Route component={NotFound}/>
       </Switch>
   )
--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,16 +1,9 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Container from "@material-ui/core/Container";
 
 export default function NotFound() {
   return (
       <React.Fragment>
-        <Container>
-          <Header/>
-          <h1>404 Not Found</h1>
-        </Container>
-        <Footer/>
+        <h1>404 Not Found</h1>
       </React.Fragment>
   )
 }
